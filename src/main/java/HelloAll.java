3381ba676d14938b4f7f009d1852dc31e46ab900
Nandyala Bhagya Sri <nandyalabhagyasri30@gmail.com>
2024-03-19 15:07:32 +0530

    adding hello all project

---
+++ src/main/java/HelloAll.java
@@ -0,0 +1,14 @@
+public class HelloAll {
+    public static void main(String[] args) {
+          System.out.println("H     H  E E E E  L        L        O O O O");
+          System.out.println("H     H  E        L        L        O     O");
+          System.out.println("H H H H  E E E E  L        L        O     O");
+          System.out.println("H     H  E        L        L        O     O");
+          System.out.println("H     H  E E E E  L L L L  L L L L  O O O O");
+          System.out.println("A A A A  L        L      ");
+          System.out.println("A     A  L        L      ");
+          System.out.println("A A A A  L        L      ");
+          System.out.println("A     A  L        L      ");
+          System.out.println("A     A  L L L L  L L L L");
+    }
+}
